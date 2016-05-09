6e85dc77500eaf1adddfacb268fa421c10197f26
mondge <709087371@qq.com>
2016-05-09 23:42:19 +0800

    del

--- pisces-learn/src/main/java/com/pisces/collect/set/Immutable.java
+++ pisces-learn/src/main/java/com/pisces/collect/set/Immutable.java
@@ -5,7 +5,6 @@ import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Lists;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by jun.xu on 16/5/9.
