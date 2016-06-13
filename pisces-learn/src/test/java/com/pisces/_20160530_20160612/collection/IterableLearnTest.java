783b29693ead33103d57596498736cd742f1b44a
mondge <709087371@qq.com>
2016-06-13 23:17:37 +0800

    pisces

---
+++ pisces-learn/src/test/java/com/pisces/_20160530_20160612/collection/IterableLearnTest.java
@@ -0,0 +1,42 @@
+package com.pisces._20160530_20160612.collection;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by jun.xu on 16/5/30.
+ */
+public class IterableLearnTest {
+    IterableLearn learn=new IterableLearn();
+
+    @Test
+    public void test1(){
+        learn.test1();
+    }
+
+    @Test
+    public void test2(){
+        learn.test2();
+    }
+
+    @Test
+    public void test3(){
+        learn.test3();
+    }
+
+    @Test
+    public void test4(){
+        learn.test4();
+    }
+
+    @Test
+    public void test5(){
+        learn.test5();
+    }
+
+    @Test
+    public void test6(){
+        learn.test6();
+    }
+}
