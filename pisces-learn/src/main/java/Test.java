a3938128d5fa97ef40b00b799eb346c789f30adc
jun.xu <jun.xu@dianping.com>
2017-09-03 23:32:15 +0800

    toString

--- pisces-learn/src/main/java/Test.java
+++ pisces-learn/src/main/java/Test.java
@@ -5,4 +5,9 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello world");
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
